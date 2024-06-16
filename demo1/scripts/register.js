7be803888914686f1fc62ec91c9dbac7d4714c27
HoshenP <hoshen.perez@gmail.com>
2024-06-16 22:23:00 +0300

    Saves new user in localStorage - WITH IMAGES WORKING

--- demo1/scripts/register.js
+++ demo1/scripts/register.js
@@ -6,67 +6,73 @@ function uid_generator() {
 
 $(document).ready(function(){
     $("#registerBtn").click(()=>{
+        
+        if ($("#username").val() === "" ||
+        $("#password").val() === "" ||
+        $("#restaurantName").val() === "" ||
+        $("#restaurantLocation").val() === "" ||
+        $("#restaurantPhone").val() === "" || !$("#restaurantPhone").prop('files')) {
+            alert("Make sure you fill all the fields!")
+        } else {
 
-
-        var images = $("#resturauntImages").prop('files');
-        var base64Images = [];
+            
+        let images = $("#resturauntImages").prop('files');
+        let base64Images = [];
 
         for (var i = 0; i < images.length; i++) {
-          (function(file) {
+            (function(file) {
+                
             var reader = new FileReader();
+            
             reader.onload = function(e) {
-              base64Images.push(e.target.result);
-              if(base64Images.length === images.length) {
+                base64Images.push(e.target.result);
+                if(base64Images.length === images.length) {
                 // All images are loaded and their base64 strings are in base64Images array
-                console.log(base64Images);
-              }
+                // console.log(base64Images);
+                
+                if(localStorage.getItem("res_users")){
+                    let tempArr = JSON.parse(localStorage.getItem("res_users"));
+                    let newRestaurnt = new Restaurant(
+                        uid_generator(),
+                        $("#username").val(),
+                        $("#password").val(),
+                        $("#restaurantName").val(),
+                        $("#restaurantLocation").val(),
+                        $("#restaurantPhone").val(),
+                        base64Images)
+                    
+                    tempArr.push(newRestaurnt)
+                    localStorage.setItem("res_users", JSON.stringify(tempArr));
+            
+                } else {
+                    let tempArr = [];
+                    let newRestaurnt = new Restaurant( uid_generator(),
+                        $("#username").val(),
+                        $("#password").val(),
+                        $("#restaurantName").val(),
+                        $("#restaurantLocation").val(),
+                        $("#restaurantPhone").val(),
+                        base64Images
+                    )
+                    
+                    tempArr.push(newRestaurnt)
+                    localStorage.setItem("res_users", JSON.stringify(tempArr));
+                }
+
+
+
+                
+                }
             };
             reader.readAsDataURL(file);
-          })(images[i]);
+            })(images[i]);
         }
+        
 
 
+        }
+        })
 
-
-    if ($("#username").val() === "" ||
-    $("#password").val() === "" ||
-    $("#restaurantName").val() === "" ||
-    $("#restaurantLocation").val() === "" ||
-    $("#restaurantPhone").val() === "" ) {
-        alert("fill in all the fields  ")
-    } else {
-       
-        if(localStorage.getItem("res_users")){
-            let tempArr = JSON.parse(localStorage.getItem("res_users"));
-            let newRestaurnt = new Restaurant( uid_generator(),
-                $("#username").val(),
-                $("#password").val(),
-                $("#restaurantName").val(),
-                $("#restaurantLocation").val(),
-                $("#restaurantPhone").val(),
-                base64Images
-            )
-             
-            tempArr.push(newRestaurnt)
-            localStorage.setItem("res_users", JSON.stringify(tempArr));
     
-         }else{
-            let tempArr = [];
-            let newRestaurnt = new Restaurant( uid_generator(),
-                $("#username").val(),
-                $("#password").val(),
-                $("#restaurantName").val(),
-                $("#restaurantLocation").val(),
-                $("#restaurantPhone").val(),
-                base64Images
-            )
-             
-            tempArr.push(newRestaurnt)
-            localStorage.setItem("res_users", JSON.stringify(tempArr));
-         }
-    }
-    })
-
-   
 })
 
